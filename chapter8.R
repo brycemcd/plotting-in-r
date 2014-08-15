29002cf99cdd86a16d8b2277725654194a0398c2
Bryce McDonnell <bryce@bridgetownint.com>
2014-08-14 22:14:26 -0400

    playing around with themes

--- chapter8.R
+++ chapter8.R
@@ -24,3 +24,26 @@ base + theme(panel.grid.minor = element_line(colour = "red", linetype = "dotted"
 
 # http://docs.ggplot2.org/0.9.2.1/theme.html for docs
 
+common_theme_options <- theme(panel.grid.minor = element_line(colour = "red", linetype = "dotted"),
+             panel.grid.major = element_line(color='blue', linetype='dashed'),
+             panel.grid.major.y = element_blank(),
+             plot.background = element_rect(fill = "green"))
+
+default <- qplot(x=cty, data=mpg, geom='histogram')
+default + common_theme_options
+
+
+common_theme_options <- theme(
+  #panel.grid = element_blank(),
+#              panel.grid.major = element_line(color='blue', linetype='dashed'),
+             panel.grid.major.y = element_line(color="lightgrey", linetype='dotted'),
+             panel.grid.minor.y = element_line(color="darkgrey", linetype='dotted'),
+             axis.text.x = element_text(position='top'),
+             axis.ticks  = element_blank(),
+             plot.background = element_rect(fill = "white"),
+             panel.background = element_rect(fill = "white")
+)
+
+default <- qplot(x=as.factor(cyl), y=cty, data=mpg, geom='boxplot')
+default
+default + common_theme_options
